643153c51b94050db265123dd1b94f459624624e
Nurdin <nurdinuzakov@gmail.com>
2021-05-29 21:58:57 +0600

    init project

---
+++ database/migrations/2021_05_29_122344_create_suppliers_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSuppliersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('suppliers', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_category');
+            $table->string('product_name');
+            $table->string('payment_methods');
+            $table->string('current_order');
+            $table->string('discount_type')->nullable();
+            $table->text('notes')->nullable();
+            $table->string('company_name');
+            $table->string('contact_first_name');
+            $table->string('contact_Last_name')->nullable();
+            $table->string('address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('state')->nullable();
+            $table->string('country')->nullable();
+            $table->integer('suppliers_phone')->unique();
+            $table->string('email')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('suppliers');
+    }
+}
