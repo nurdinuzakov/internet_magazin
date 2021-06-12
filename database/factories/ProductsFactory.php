c2780b7320e917958a82cff57f8a6874c2829396
Nurdin <nurdinuzakov@gmail.com>
2021-06-12 14:47:18 +0600

    added factory faker

---
+++ database/factories/ProductsFactory.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\products;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Psy\Util\Str;
+
+class ProductsFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Products::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $images = [
+            '/images/electronic-products/iphone12.jpg',
+            'images/home/mylomoyushie.jpg', '/images/food-products/frima.jpg',
+            '/images/electronic-products/poco.jpg'
+        ];
+        return [
+            'supplier_id'=>null,
+            'name' => $this->faker->name(),
+            'description' => $this->faker->text(),
+            'category_id' => $this->faker->numberBetween(1,6),
+            'quantity' => $this->faker->numberBetween(1, 10),
+            'price' => $this->faker->numberBetween(100, 200),
+            'images' => $this->faker->randomElement($images),
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ];
+    }
+}
---
+++ database/seeders/ProductsSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Database\Factories\ProductsFactory;
+use Faker\Factory;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\Products;
+class ProductsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Products::factory()->count(20)->create();
+
+//        $faker = Factory::create();
+//
+//        for ($i = 0; $i < 20; $i++) {
+//            Products::create([
+//                'name' => $faker->name
+//            ]);
+//        }
+    }
+}
